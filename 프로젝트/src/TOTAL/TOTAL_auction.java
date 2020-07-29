08ffe1f567846db67d61c39c9f99736ec08541f0
SMT065 <SMT065@218.157.111.106>
2020-07-29 10:54:45 +0900

    운비아ㅟㅏㅁㄴ욺ㄴㅇ루ㅜㅇㅁㄴ라ㅜ

--- 프로젝트/src/TOTAL/TOTAL_auction.java
+++ 프로젝트/src/TOTAL/TOTAL_auction.java
@@ -6,11 +6,13 @@ import DAO.DAO_auction;
 
 public class TOTAL_auction {
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		DAO_auction dao = new DAO_auction();
-		
-		
+
+		System.out.println("테스트 중입니다");
+		System.out.println("으아아악");
+
 	}
 
 }
