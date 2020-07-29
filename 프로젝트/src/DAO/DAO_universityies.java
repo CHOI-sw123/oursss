481b69756b5941ea7bf9a3683defe72da3491847
SMT065 <SMT065@218.157.111.106>
2020-07-29 12:33:12 +0900

    최성우 수정

--- 프로젝트/src/DAO/DAO_universityies.java
+++ 프로젝트/src/DAO/DAO_universityies.java
@@ -9,149 +9,36 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class DAO_universityies {
-	// 필드 --->DAO클래스 전역에 닿을 수 있게 생성
-		private Connection conn = null;
-		private PreparedStatement psmt = null;
-		private ResultSet rs = null;
+	private Connection conn = null;
+	private PreparedStatement psmt = null;
+	private ResultSet rs = null;
 
-		// 데이터베이스와 연결하는 메소드 생성
-		private void getConnection() {
-			String university = "";
-			
-			
-			try {
-				// 1. 드라이버 동적로딩!
-				Class.forName("oracle.jdbc.driver.OracleDriver");
-				conn = DriverManager.getConnection(university);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
-		}
-
-		// 데이터베이스 자원을 반납하는 닫기 메소드
-		private void close() {
-			try {
-				if (rs != null)
-					rs.close();
-				if (psmt != null)
-					psmt.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
-		}
-		public VO login(VO vo) {
-			VO result = null;
-			try {
-				getConnection();
-				String sql = "select name,age from members where id =? and pw = ?";
-				psmt = conn.prepareStatement(sql);
-				psmt.setString(1, vo.getId());
-				psmt.setString(2, vo.getPw());
-				rs = psmt.executeQuery();
-				while (rs.next()) {
-					String name = rs.getString(1);
-					int age = rs.getInt(2);
-					result =new VO(name, age);
-				}
-
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} finally {
-				close();
-			}
-			return result;
-		}
-
-		// 회원가입 메소드
-		public int insert(VO vo) {
-			int cnt = 0;
-			try {
-				getConnection();
-				String sql = "insert into members values(?,?,?,?)";
-				psmt = conn.prepareStatement(sql);
-				psmt.setString(1, vo.getId());
-				psmt.setString(2, vo.getPw());
-				psmt.setString(3, vo.getName());
-				psmt.setInt(4, vo.getAge());
-				cnt = psmt.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} finally {
-				close();
-			}
-
-			return cnt;
-		}
-
-		public int update(String id, String pw, String newId) {
-			int cnt = 0;
-
-			try {
-				getConnection();
-				String sql = "update members set id = ? where id =? and pw = ?";
-				psmt = conn.prepareStatement(sql);
-				psmt.setString(1, newId);
-				psmt.setString(2, id);
-				psmt.setString(3, pw);
-
-				cnt = psmt.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} finally {
-				close();
-			}
-			return cnt;
-		}
-
-		public int delete(VO vo) {
-			int cnt = 0;
-
-			try {
-				getConnection();
-				String sql = "DELETE FROM MEMBERS WHERE id = ? and pw = ?";
-				psmt = conn.prepareStatement(sql);
-				psmt.setString(1, vo.getId());
-				psmt.setString(2, vo.getPw());
-
-				cnt = psmt.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} finally {
-				close();
-			}
-			return cnt;
-		}
-
-		public ArrayList<VO> allSelect() {
-
-			ArrayList<VO> list = new ArrayList<VO>();
-			
-			try {
-				getConnection();
-				String sql = "SELECT * FROM MEMBERS";
-				psmt = conn.prepareStatement(sql);
-				rs = psmt.executeQuery();
-				int num = 1;
-				while (rs.next()) {
-					String id = rs.getString(1);
-					String pw = rs.getString(2);
-					String name = rs.getString(3);
-					int age = rs.getInt(4);
-					VO vo = new VO(id, pw, name, age);
-					list.add(vo);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} finally {
-				close();
-			}
-			return list;
+	private void getConnection() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "ours";
+		String password = "ours";
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 
 	}
+
+	// 데이터베이스 자원을 반납하는 닫기 메소드
+	private void close() {
+		try {
+			if (rs != null)
+				rs.close();
+			if (psmt != null)
+				psmt.close();
+			if (conn != null)
+				conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
